58b5619684007269202d51362d9693c56fb41522
AjaxDemo <13472249721@163.com>
2019-10-28 17:41:47 +0800

    Update HelloWorld.java
    other edit

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello_world");
                 System.out.println("add dev_temp");  
+		System.out.println("other edit"); 
 	}
 }
