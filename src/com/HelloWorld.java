ceadb2c0aadf8fd6717fef349e74795e3b7d1a79
zhangmo <zhangmo@vknowtech.cn>
2019-10-28 18:09:31 +0800

    新开发

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello_world");
                 System.out.println("add dev_temp");  
-		System.out.println("other edit"); 
+		System.out.println("other edit");
+		System.out.println("本地新开发任务");
 	}
 }
