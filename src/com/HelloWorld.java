a34615a2648558131a126cdc7df22abf40ed3429
zhangmo <zhangmo@vknowtech.cn>
2019-10-29 10:12:46 +0800

    本地修改111222

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 		System.out.println("hello_world");
 		System.out.println("add dev_temp");
 		System.out.println("other edit1111222333");
-		System.out.println("本地修改111");
+		System.out.println("本地修改111222");
 	}
 }
