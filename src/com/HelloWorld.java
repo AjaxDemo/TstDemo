32aaf319c4c51a370f759fb34074358c5c96e17e
AjaxDemo <13472249721@163.com>
2020-02-08 14:40:07 +0800

    del2

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		System.out.println("hello_world");
                 System.out.println("add dev_temp");
 		System.out.println("20200110 change code");
-		System.out.println("master zm chage");
+		 
 	
 		
 	}
