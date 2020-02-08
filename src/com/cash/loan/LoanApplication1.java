d7d9af82044e623a6debb272fa4030089f740b1d
zm <zhangmo@greatsoft.net>
2020-02-08 14:23:16 +0800

    remote111 add

---
+++ src/com/cash/loan/LoanApplication1.java
@@ -0,0 +1,15 @@
+package com.cash.loan;
+
+public class LoanApplication {
+
+    public void loanCash(){
+        System.out.println("新加分支dev1");
+
+        System.out.println("master zm change");
+        System.out.println("20200110 change sth");
+        System.out.println("20200110 change sth");
+        System.out.println("20200110 change sth");
+        System.out.println("change sth remote");
+        System.out.println("change sth remote111");
+    }
+}
