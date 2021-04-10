feb328af85baf23f71a5a471c23f8894b94668fb
AjaxDemo <13472249721@163.com>
2021-04-10 10:47:15 +0800

    Update HelloWorld.java

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello_world");
 		System.out.println("add dev_temp");
-		System.out.println("other edit1111222333444");
-		System.out.println("本地修改111222");
+		System.out.println("本地修改333");
 	}
 }
