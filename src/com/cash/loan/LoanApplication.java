f877490f1c63677d0b5dd28a55a6e14713e34f4d
AjaxDemo <13472249721@163.com>
2021-04-22 17:11:03 +0800

    Update LoanApplication.java

--- src/com/cash/loan/LoanApplication.java
+++ src/com/cash/loan/LoanApplication.java
@@ -5,6 +5,7 @@ public class LoanApplication {
     public void loanCash(){
         System.out.println("新加分支dev1");
         System.out.println("测试冲突");
+        //---
     }
 
     public void newChange() {
